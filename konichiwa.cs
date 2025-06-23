b6c60a7468d0cd62fd59bff2d03dd61e8870d4e4
nhatdu66 <galaxya04nnd@gmail.com>
2025-06-23 15:21:57 +0700

    Add files via upload

---
+++ konichiwa.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Xin chào! Bạn tên là gì?");
+        string ten = Console.ReadLine();
+
+        Console.WriteLine($"Rất vui được gặp bạn, {ten}!");
+    }
+}
