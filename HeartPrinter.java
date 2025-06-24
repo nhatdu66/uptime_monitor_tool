6475c1588bafbb045b108fdf0438a83aa6381afb
nhatdu66 <galaxya04nnd@gmail.com>
2025-06-24 19:20:39 +0700

    Add files via upload

---
+++ HeartPrinter.java
@@ -0,0 +1,23 @@
+public class HeartPrinter {
+    public static void main(String[] args) {
+        System.out.println("    *****       *****    ");
+        System.out.println("   *     *     *     *   ");
+        System.out.println("  *       *   *       *  ");
+        System.out.println(" *         * *         * ");
+        System.out.println("*                       *");
+        System.out.println("*    I love you 999!    *");
+        System.out.println("*                       *");
+        System.out.println(" *                     * ");
+        System.out.println("  *                   *  ");
+        System.out.println("   *                 *   ");
+        System.out.println("    *               *    ");
+        System.out.println("     *             *     ");
+        System.out.println("      *           *      ");
+        System.out.println("       *         *       ");
+        System.out.println("        *       *        ");
+        System.out.println("         *     *         ");
+        System.out.println("          *   *          ");
+        System.out.println("           * *           ");
+        System.out.println("            *            ");
+    }
+}
