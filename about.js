27745b12f4383a940ec400005772113c37cf40f0
nhatdu66 <galaxya04nnd@gmail.com>
2025-08-09 08:17:53 +0700

    Add files via upload

---
+++ about.js
@@ -0,0 +1,3 @@
+document.getElementById("btn-about").addEventListener("click", function() {
+    alert("Bạn đang ở TRANG GIỚI THIỆU!");
+});
---
+++ home.js
@@ -0,0 +1,3 @@
+document.getElementById("btn-home").addEventListener("click", function() {
+    alert("Bạn đang ở TRANG CHỦ!");
+});
